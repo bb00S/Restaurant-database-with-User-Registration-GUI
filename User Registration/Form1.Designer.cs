a95a67861b55dc57abb2243a10e9f31e26d98dd8
bb00S <bokiborovcanin2000@gmail.com>
2023-07-12 18:14:05 +0200

    Add project files.

---
+++ User Registration/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace User_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtImeKorisnika = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrezimeKorisnika = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdresa = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtGrad = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtDrzava = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIDKorisnika = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime korisnika";
+            // 
+            // txtImeKorisnika
+            // 
+            this.txtImeKorisnika.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImeKorisnika.Location = new System.Drawing.Point(151, 176);
+            this.txtImeKorisnika.Name = "txtImeKorisnika";
+            this.txtImeKorisnika.Size = new System.Drawing.Size(172, 29);
+            this.txtImeKorisnika.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Prezime korisnika";
+            // 
+            // txtPrezimeKorisnika
+            // 
+            this.txtPrezimeKorisnika.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrezimeKorisnika.Location = new System.Drawing.Point(151, 211);
+            this.txtPrezimeKorisnika.Name = "txtPrezimeKorisnika";
+            this.txtPrezimeKorisnika.Size = new System.Drawing.Size(172, 29);
+            this.txtPrezimeKorisnika.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(89, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(151, 281);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(172, 29);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(469, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Adresa";
+            // 
+            // txtAdresa
+            // 
+            this.txtAdresa.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdresa.Location = new System.Drawing.Point(535, 28);
+            this.txtAdresa.Multiline = true;
+            this.txtAdresa.Name = "txtAdresa";
+            this.txtAdresa.Size = new System.Drawing.Size(172, 42);
+            this.txtAdresa.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(33, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Korisnicko ime";
+            // 
+            // txtKorisnickoIme
+            // 
+            this.txtKorisnickoIme.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKorisnickoIme.Location = new System.Drawing.Point(151, 73);
+            this.txtKorisnickoIme.Name = "txtKorisnickoIme";
+            this.txtKorisnickoIme.Size = new System.Drawing.Size(172, 29);
+            this.txtKorisnickoIme.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(78, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 21);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Lozinka";
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLozinka.Location = new System.Drawing.Point(151, 118);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.Size = new System.Drawing.Size(172, 29);
+            this.txtLozinka.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(535, 218);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(172, 57);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Prijava";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(513, 207);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 21);
+            this.label9.TabIndex = 0;
+            // 
+            // txtGrad
+            // 
+            this.txtGrad.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGrad.Location = new System.Drawing.Point(535, 128);
+            this.txtGrad.Name = "txtGrad";
+            this.txtGrad.Size = new System.Drawing.Size(172, 29);
+            this.txtGrad.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(483, 131);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 21);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Grad";
+            // 
+            // txtDrzava
+            // 
+            this.txtDrzava.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDrzava.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtDrzava.Location = new System.Drawing.Point(535, 173);
+            this.txtDrzava.Name = "txtDrzava";
+            this.txtDrzava.Size = new System.Drawing.Size(172, 29);
+            this.txtDrzava.TabIndex = 5;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(469, 173);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 21);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Drzava";
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefon.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.txtTelefon.Location = new System.Drawing.Point(151, 246);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(172, 29);
+            this.txtTelefon.TabIndex = 7;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(78, 246);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(59, 21);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Telefon";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(535, 92);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(172, 20);
+            this.dateTimePicker.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(409, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Datum rodjenja";
+            // 
+            // txtIDKorisnika
+            // 
+            this.txtIDKorisnika.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIDKorisnika.Location = new System.Drawing.Point(151, 25);
+            this.txtIDKorisnika.Name = "txtIDKorisnika";
+            this.txtIDKorisnika.Size = new System.Drawing.Size(172, 29);
+            this.txtIDKorisnika.TabIndex = 13;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(49, 25);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(92, 21);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "ID korisnika";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 410);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtIDKorisnika);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtDrzava);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtGrad);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtAdresa);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrezimeKorisnika);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKorisnickoIme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtImeKorisnika);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registracija korisnika ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtImeKorisnika;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrezimeKorisnika;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdresa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKorisnickoIme;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtGrad;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtDrzava;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtIDKorisnika;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ User Registration/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace User_Registration
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Data Source=DESKTOP-04PDE12;Initial Catalog=\"Baza Restoran\";Integrated Security=True";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("InsertKorisnik1", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    // Set parameter values
+                        command.Parameters.AddWithValue("@id_korisnika", txtIDKorisnika.Text);
+                        command.Parameters.AddWithValue("@korisnicko_ime", txtKorisnickoIme.Text);
+                        
+                        command.Parameters.AddWithValue("@lozinka", txtLozinka.Text);
+                        command.Parameters.AddWithValue("@ime", txtImeKorisnika.Text);
+                        command.Parameters.AddWithValue("@prezime", txtPrezimeKorisnika.Text);
+                        command.Parameters.AddWithValue("@telefon", txtTelefon.Text);
+                        command.Parameters.AddWithValue("@email", txtEmail.Text);
+                        command.Parameters.AddWithValue("@adresa", txtAdresa.Text);
+                        
+                        command.Parameters.AddWithValue("@datum_rodjenja", dateTimePicker.Value);
+                        command.Parameters.AddWithValue("@zemlja", txtDrzava.Text);
+                        command.Parameters.AddWithValue("@grad", txtGrad.Text);
+
+                        command.ExecuteNonQuery();
+                }
+            }
+        }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
